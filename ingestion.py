3a8bda088bfdb960d45cb26111970db67daa4d86
IAmGuru <guruprasad019@yahoo.co.in>
2025-04-28 15:28:50 +0530

    changes commit for batch_size during ingestion
    commit changes

--- ingestion.py
+++ ingestion.py
@@ -11,7 +11,7 @@ embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
 
 
 def ingest_docs():
-    loader = ReadTheDocsLoader("langchain-docs/langchain.readthedocs.io/en/v0.1")
+    loader = ReadTheDocsLoader("langchain-docs/api.python.langchain.com/en/latest", encoding='utf-8')
 
     raw_documents = loader.load()
     print(f"loaded {len(raw_documents)} documents")
@@ -20,14 +20,22 @@ def ingest_docs():
     documents = text_splitter.split_documents(raw_documents)
     for doc in documents:
         new_url = doc.metadata["source"]
-        new_url = new_url.replace("langchain-docs", "https:/")
+        # print("new_url",new_url)
+        new_url = new_url.replace("langchain-docs", "https:/").replace("\\","/")
+        # print("updated new url: ", new_url)
         doc.metadata.update({"source": new_url})
 
     print(f"Going to add {len(documents)} to Pinecone")
-    PineconeVectorStore.from_documents(
-        documents, embeddings, index_name="langchain-doc-index"
-    )
+    BATCH_SIZE = 250
+    for i in range(0, len(documents),BATCH_SIZE):
+        doc_ = documents[i:i+BATCH_SIZE]
+        print("documents reference",i,i+BATCH_SIZE)
+        PineconeVectorStore.from_documents(
+            doc_, embeddings, index_name="langchain-doc-index"
+        )
     print("****Loading to vectorstore done ***")
+    # for doc in documents[0:10]:
+    #     print("\n************",doc)
 
 
 if __name__ == "__main__":
